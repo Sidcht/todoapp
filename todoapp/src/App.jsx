75b032e784b1937f573d6a28cf631dc99759b7b7
Sidcht <veldabu1327@gmail.com>
2024-10-03 14:28:05 +0800

    initial commit

---
+++ todoapp/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Todo from "./components/Todo";
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
